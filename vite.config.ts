feddd1dc772c0dab2d7dfa027a4ec069f636fac7
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2024-07-15 23:51:27 +0900

    chore: path alias を追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [{ find: "@/", replacement: `${__dirname}/src/` }],
+  },
 });
