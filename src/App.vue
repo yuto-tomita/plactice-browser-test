50a196b2a3d9db5450e3e96821ed713ac11e1202
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2024-07-21 19:11:09 +0900

    refactor: style 関連ファイルの削除

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-
+import LoginPage from "@/pages/LoginPage.vue";
 </script>
 
 <template>
-  <LoginPage></LoginPage>
+  <LoginPage />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
