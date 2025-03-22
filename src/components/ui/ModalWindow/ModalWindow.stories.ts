e0459f007de16f3fffffadd03025b38dd09badb1
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2025-03-22 19:56:29 +0900

    test: test 修正

--- src/components/ui/ModalWindow/ModalWindow.stories.ts
+++ src/components/ui/ModalWindow/ModalWindow.stories.ts
@@ -24,27 +24,3 @@ export const Basic: Story = {
     modelValue: true,
   },
 };
-
-// NOTE: 通常のUI確認用のStoryと同一ファイルにインタラクションテストを書いてしまうと、確認用UIタブ中にテストの用のStoryが生成されてしまうため微妙な気がする
-export const TestModalClose: Story = {
-  render: (args) => ({
-    components: { ModalWindow },
-    setup() {
-      return { args };
-    },
-    template: "<ModalWindow v-bind='args' />",
-  }),
-  args: {
-    modelValue: true,
-  },
-  play: async ({ canvasElement, step }) => {
-    const canvas = within(canvasElement);
-    await step("close ボタンをクリックしたらモーダルが表示されなくなること", async () => {
-      await fireEvent.click(canvas.getByTestId("close-button"));
-
-      expect(canvas.queryByRole("dialog")).toBeNull();
-    });
-
-    await step("背景をクリックしてもモーダルが閉じないこと", async () => {});
-  },
-};
--- src/components/ui/ModalWindow/ModalWindow.test.stories.ts
+++ src/components/ui/ModalWindow/ModalWindow.test.stories.ts
@@ -3,7 +3,17 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import { expect, fireEvent, waitFor, within } from "@storybook/test";
 
 import ModalWindow from "./ModalWindow.vue";
+// NOTE: Interaction Test 良いところポイント
+// - jsdom 等を使用したテスト用のシミュレーション環境ではなく、実際にブラウザにレンダリングすることによってユーザーと近い環境でテストを行うことができる
+// - Storybook コンソールが見やすい
+// - Storybook コンソール上からイベントをステップ実行できるため、テストが可視化されてわかりやすい(フロントテストに慣れていなくてもデバッグしやすいのでは)
+// template を拡張できるため、よりユースケースに沿ったコンポーネントテストが書ける
 
+// NOTE: Interaction Test 微妙ポイント
+// - console.log が直感的でない(throw new Error をして確認する必要がある)
+// - Step Function を使用すると本来落ちるはずのテストが Pass する
+// - (やらなくても良いが) test用のStoryファイルとtest用のStoryファイルを分ける必要がある(二重管理が必要)
+// - テストごとに Story 環境を作る必要があるため冗長になりがち
 const meta: Meta<typeof ModalWindow> = {
   title: "interaction test/ui/ModalWindow",
   component: ModalWindow,
@@ -35,17 +45,19 @@ export const 背景をクリックしてもモーダルが閉じないこと: St
   },
 };
 
-export const モーダルが閉じること: Story = {
+export const Escapeキーでモーダルが閉じること: Story = {
   ...defaultComponent,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    await fireEvent.click(canvas.getByTestId("close-button"));
+    await fireEvent.keyDown(canvas.getByRole("dialog"), { key: "Escape", code: "Escape", keyCode: 27, charCode: 27 });
 
-    expect(canvas.queryByRole("dialog")).toBeNull();
+    await waitFor(() => {
+      expect(canvas.queryByRole("dialog")).toBeNull();
+    });
   },
 };
 
-export const 複数存在するフォームのうち先頭のフォームがアクティブであること: Story = {
+export const 複数存在するタブフォーカス可能な要素のうち一番先頭要素にフォーカスが当たること: Story = {
   render: (args) => ({
     components: { ModalWindow },
     setup() {
@@ -65,35 +77,11 @@ export const 複数存在するフォームのうち先頭のフォームがア�
   play: async () => {
     const activeForm = document.activeElement as HTMLInputElement;
 
-    expect(activeForm.placeholder).toEqual("test form");
+    expect(activeForm.type).toEqual("button");
   },
 };
 
-export const 先頭のフォームがdisableなフォームの場合disableの次のフォームがアクティブであること: Story = {
-  render: (args) => ({
-    components: { ModalWindow },
-    setup() {
-      return { args };
-    },
-    template: `<ModalWindow v-bind='args'>
-      <label for="test-form">test disable form</label><br>
-      <input type="text-disable-form" disabled /><br>
-
-      <label for="test-form">test form</label><br/>
-      <input type="text" id="test-form" placeholder="test form" />
-    </ModalWindow>`,
-  }),
-  args: {
-    modelValue: true,
-  },
-  play: async () => {
-    const activeForm = document.activeElement as HTMLInputElement;
-
-    expect(activeForm.placeholder).toEqual("test form");
-  },
-};
-
-export const 二番目のフォームをアクティブにした状態でモーダルを閉じてから再度モーダルを開くと先頭のフォームがアクティブとなること: Story =
+export const フォーカス可能な最後の要素をアクティブにした状態でモーダルを閉じてから再度モーダルを開くと先頭のフォーカス可能要素がアクティブとなること: Story =
   {
     render: (args) => ({
       components: { ModalWindow },
@@ -123,12 +111,46 @@ export const 二番目のフォームをアクティブにした状態でモー�
       canvas.getByPlaceholderText("test form2").focus();
       expect((document.activeElement as HTMLInputElement).placeholder).toEqual("test form2");
 
-      await fireEvent.click(canvas.getByTestId("close-button"));
+      await fireEvent.keyDown(canvas.getByRole("dialog"), { key: "Escape" });
       expect(canvas.queryByRole("dialog")).toBeNull();
 
       await fireEvent.click(canvas.getByText("click me"));
       await waitFor(() => {
-        expect((document.activeElement as HTMLInputElement).placeholder).toEqual("test form");
+        expect((document.activeElement as HTMLInputElement).type).toEqual("button");
+      });
+    },
+  };
+
+export const 親要素にアクティブなフォームが存在してもモーダルを開いた際にモーダル内のフォームがアクティブになること: Story =
+  {
+    render: (args) => ({
+      components: { ModalWindow },
+      setup() {
+        return { args };
+      },
+      template: `
+      <div>
+        <label for="test-form">test form</label><br/>
+        <input type="text" id="test-form" placeholder="test form" /><br>
+        <button type="button" @click="args.modelValue = true">click me</button>
+
+        <ModalWindow v-model='args.modelValue'>
+          <label for="test-form2">test form 2</label><br/>
+          <input type="text" id="test-form2" placeholder="test form2" />
+        </ModalWindow>
+      </div>`,
+    }),
+    args: {
+      modelValue: false,
+    },
+    play: async ({ canvasElement }) => {
+      const canvas = within(canvasElement);
+      canvas.getByPlaceholderText("test form").focus();
+      expect((document.activeElement as HTMLInputElement).placeholder).toEqual("test form");
+
+      await fireEvent.click(canvas.getByText("click me"));
+      await waitFor(() => {
+        expect((document.activeElement as HTMLInputElement).type).toEqual("button");
       });
     },
   };
